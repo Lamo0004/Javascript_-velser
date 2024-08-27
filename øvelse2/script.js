6ba36bd65e999371b74d4c41f3d3ff854046cb89
Laura Mølnitz <lamo0004@stud.kea.dk>
2024-08-27 21:16:12 +0200

    øvelse 2

---
+++ øvelse2/script.js
@@ -0,0 +1,13 @@
+// Henter det aktuelle timetal
+var timetal = new Date().getHours();
+
+// Funktion der udskriver hilsen baseret på tidspunktet
+if (timetal >= 5 && timetal < 10) {
+  console.log("Godmorgen");
+} else if (timetal >= 10 && timetal < 18) {
+  console.log("Goddag");
+} else if (timetal >= 18 && timetal < 24) {
+  console.log("Godaften");
+} else {
+  console.log("Godnat");
+}
